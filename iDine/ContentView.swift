a964a66bc1e1e359bbfd7534a5bf91579d480faa
defean <bin4tech@unitybox.de>
2020-06-17 14:22:31 +0200

    Initial Version

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Andreas on 17.06.20.
+//  Copyright © 2020 Andreas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
